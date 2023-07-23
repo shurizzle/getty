11f5a504be130036d9242b4f9d9747ffdc9653d8
shurizzle <me@shurizzle.dev>
2023-07-23 05:46:57 +0200

    Add support for android

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(all(not(feature = "std"), not(test)), no_std)]
 
-#[cfg_attr(target_os = "linux", path = "linux/mod.rs")]
+#[cfg_attr(any(target_os = "linux", target_os = "android"), path = "linux/mod.rs")]
 #[cfg_attr(
     any(
         target_os = "macos",
